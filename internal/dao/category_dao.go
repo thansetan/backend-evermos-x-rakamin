35dffe400931ada8e9c69d8ee193faaa1ce4ce41
thansetan <62317096+thansetan@users.noreply.github.com>
2023-06-25 13:18:33 +0700

    prevent category deletion when products > 0

--- internal/dao/category_dao.go
+++ internal/dao/category_dao.go
@@ -1,9 +1,22 @@
 package dao
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
 	gorm.Model
 	CategoryName string `json:"nama_kategori"`
 	Product      *Product
+}
+
+func (c *Category) BeforeDelete(tx *gorm.DB) error {
+	var count int64
+	tx.Model(&Product{}).Where("category_id = ?", c.ID).Count(&count)
+	if count > 0 {
+		return errors.New("can't delete category with associated product")
+	}
+	return nil
 }
